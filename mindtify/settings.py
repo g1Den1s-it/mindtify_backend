0787b37820cb54a8250825f9ac3fb2ec2faad2d6
Denys Sanotskyi <g1den1splay@gmail.com>
2023-09-07 10:53:32 +0300

    added media folder

--- mindtify/settings.py
+++ mindtify/settings.py
@@ -128,12 +128,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+AUTH_USER_MODEL = "user.User"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- mindtify/urls.py
+++ mindtify/urls.py
@@ -14,22 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
+
+
+from mindtify import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 
-    path('api/user/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/user/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-
     path('api/user/', include('user.urls')),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
