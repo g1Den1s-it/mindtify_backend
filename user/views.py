3adb872f731b9c635c105915b676d4ca67adccec
Denys Sanotskyi <g1den1splay@gmail.com>
2023-09-07 10:55:36 +0300

    added custom TokenObtainPairView

--- user/views.py
+++ user/views.py
@@ -2,8 +2,30 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth.hashers import make_password
+from django.shortcuts import get_object_or_404
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from user.serializers import UserSerializer
+from user.models import User
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            data = request.data
+            user = get_object_or_404(User, username=data['username'])
+            user_data = {
+                'user_id': user.id,
+                'username': user.username,
+                'image': user.image.url
+            }
+            response.data['user'] = user_data
+
+        return response
 
 
 class RegistrationView(generics.CreateAPIView):
