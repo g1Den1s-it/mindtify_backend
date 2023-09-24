86920f0be0b87b36238d57901bd1f731587404c7
Denys Sanotskyi <g1den1splay@gmail.com>
2023-09-24 16:03:27 +0300

    added home modul to project

--- mindtify/settings.py
+++ mindtify/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'user',
+    'home',
 ]
 
 MIDDLEWARE = [
--- mindtify/urls.py
+++ mindtify/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 
     path('api/user/', include('user.urls')),
+    path('', include('home.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
