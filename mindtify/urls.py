7945b98caab2a3867803c872e76cca5bc8d6912e
Denys Sanotskyi <g1den1splay@gmail.com>
2023-09-26 23:58:10 +0300

    update url static and media

--- mindtify/urls.py
+++ mindtify/urls.py
@@ -28,4 +28,7 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('', include('home.urls')),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
