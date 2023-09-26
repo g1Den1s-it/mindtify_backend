bf9f67b9dc8e9341a5478735f50fc64d7d1476e9
Denys Sanotskyi <g1den1splay@gmail.com>
2023-09-26 23:50:08 +0300

    added script to docker compose

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+python3 manage.py makemigrations
+
+python3 manage.py migrate
+
+if [ ! -f /app/static/.last_modified ]; then
+    python3 manage.py collectstatic
+    touch /app/static/.last_modified
+fi
+
+gunicorn mindtify.wsgi:application --bind 0.0.0.0:8000
